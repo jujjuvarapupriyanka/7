8b4368e41a3adc55f980fa92237a9686de63a058
jujjuvarapupriyanka <jujjuvarapu.priyanka051@gmail.com>
2024-04-13 09:07:17 -0700

    task done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import CustomButton from './components/button/button';
+import Realimage from './components/image/image';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CustomButton></CustomButton>
+      <CustomButton></CustomButton>
+      <CustomButton></CustomButton>
+      <Realimage></Realimage>
+      
+
     </div>
   );
 }
---
+++ src/components/button/button.js
@@ -0,0 +1,7 @@
+function CustomButton(){
+    return(
+
+       <button>login</button>
+    )
+}
+export default CustomButton;
